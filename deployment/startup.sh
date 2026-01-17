43fcb750e457829363af14d2714281331e691eb2
Tom Boone <tbc@tomboone.com>
2026-01-17 14:00:01 -0500

    tweak startup script

--- deployment/startup.sh
+++ deployment/startup.sh
@@ -6,5 +6,5 @@ cp deployment/default /etc/nginx/sites-enabled/default
 service nginx restart
 
 for site in default jeannebriggs.com rss.tomboone.com; do
-  vendor/bin/drush deploy --uri=$site
+  php vendor/bin/drush deploy --uri=$site
 done
